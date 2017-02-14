95842521e2a6a2653d650690c5973f5757fce089
Michael Opaska <mopaska1106@gmail.com>
2017-02-14 05:04:36 -0500

    Joined tables to get subscription name returned.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,9 @@ class MembersController < ApplicationController
 
   # GET /members
   def index
-    @members = Member.all
+    @members = Member
+                .select('members.name as name', 'members.email as email', 'members.phone as phone','subscriptions.name as sub')
+                .joins(:subscription)
 
     render json: @members
   end
