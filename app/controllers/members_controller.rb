03a34123303199571b649e35adfa65f08b0fab2e
Michael Opaska <mopaska1106@gmail.com>
2017-02-14 19:32:09 -0500

    Returning the subscription price as well

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
   # GET /members
   def index
     @members = Member
-                .select('members.*', 'subscriptions.name as subscription_level')
+                .select('members.*', 'subscriptions.name as subscription_level', 'subscriptions.price as subscription_price')
                 .joins(:subscription)
 
     render json: @members
